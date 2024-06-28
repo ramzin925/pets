5bf48c73301efcc01e01aac81095a2f4e5b191c0
Pedro-Ribeiro_925 <165576520+ramzin925@users.noreply.github.com>
2024-06-28 11:58:24 -0300

    Add files via upload

--- pet-shop.js
+++ pet-shop.js
@@ -3,3 +3,23 @@
 function redirecionar(){
     window.location.href = "index.html"
 }
+
+//Perfil-Cuidador
+
+const input = document.getElementById('foto-de-perfil');
+const preview = document.getElementById('previa');
+
+input.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function() {
+            preview.style.display = 'block';
+            preview.src = reader.result;
+        }
+        reader.readAsDataURL(file);
+    } else {
+        preview.style.display = 'none';
+        preview.src = '#';
+    }
+});
